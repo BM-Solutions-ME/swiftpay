e5aec080b3bb38982d9ac6e085a97619cabd44f6
Bruno Mota <bruno91.m@gmail.com>
2025-08-18 17:15:26 -0300

    feat: Valida conta de novos usuários, atualizando o status para ativo.

--- source/Infra/Controllers/Api/SignupController.php
+++ source/Infra/Controllers/Api/SignupController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Source\Infra\Controllers\Api;
 
 use Source\App\Services\SignupService;
+use Source\App\Services\ValidateNewAccountService;
 use Source\Framework\Support\CallbackHandler;
 use Source\Infra\Repositories\SignupRepository;
+use Source\Infra\Repositories\UserRepository;
 
 class SignupController
 {
@@ -19,6 +21,23 @@ class SignupController
         try {
             $newUser = (new SignupService(new SignupRepository))->handle($data);
             (new CallbackHandler())->output(code: 201, response: ["data" => $newUser->toArray()]);
+        } catch (\Throwable $e) {
+            (new CallbackHandler())->set(
+                $e->getCode(),
+                "error",
+                $e->getMessage()
+            )->output();
+        }
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     * @return void
+    */
+    public function validateNewAccount(array $data): void
+    {
+        try {
+            (new ValidateNewAccountService(new UserRepository()))->handle($data);
         } catch (\Throwable $e) {
             (new CallbackHandler())->set(
                 $e->getCode(),
