b99f999bec346b87c294eb9b76cf2a59ada4cfba
Bruno Mota <bruno91.m@gmail.com>
2025-08-21 10:26:05 -0300

    feat: Cadastro de empreasa.

---
+++ source/App/Usecases/Company/CompanyRegister/CompanyRegisterInputData.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Source\App\Usecases\Company\CompanyRegister;
+
+final class CompanyRegisterInputData
+{
+    public function __construct(
+        private readonly int $userId,
+        private readonly string $publicName,
+        private readonly string $legalName,
+        private readonly string $document,
+        private readonly ?string $dateFoundation = null
+    ) {}
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getPublicName(): string
+    {
+        return $this->publicName;
+    }
+
+    public function getLegalName(): string
+    {
+        return $this->legalName;
+    }
+
+    public function getDocument(): string
+    {
+        return $this->document;
+    }
+
+    public function getDateFoundation(): ?string
+    {
+        return $this->dateFoundation;
+    }
+}
