f7736b96c684eb7c4a6f20538e1dda072a615120
Bruno Mota <bruno91.m@gmail.com>
2026-01-29 10:40:07 -0300

    refactor: O controler de transferência executa diretamente o usecase.

---
+++ source/App/Usecases/Transfer/ExecuteTransfer/ExecuteTransferUsecase.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Source\App\Usecases\Transfer\ExecuteTransfer;
+
+use Exception;
+use Source\App\Contracts\CurlRequestInterface;
+use Source\App\Usecases\Transfer\CreateTransfer\CreateTransferInput;
+use Source\App\Usecases\Transfer\CreateTransfer\CreateTransferUsecase;
+use Source\App\Usecases\Wallet\DecreaseBalance\DecreaseBalanceUsecase;
+use Source\App\Usecases\Wallet\IncreaseBalance\IncreaseBalanceUsecase;
+use Source\Domain\Entities\User;
+use Source\Domain\Repositories\TransferRepositoryInterface;
+use Source\Domain\Repositories\WalletRepositoryInterface;
+use Source\Domain\Repositories\UserRepositoryInterface;
+
+final class ExecuteTransferUsecase
+{
+    public function __construct(
+        private readonly TransferRepositoryInterface $transferRepo,
+        private readonly WalletRepositoryInterface $walletRepo,
+        private readonly UserRepositoryInterface $userRepo,
+        private readonly CurlRequestInterface $http
+    ) {}
+
+    public function handle(User $payer, CreateTransferInput $input): ExecuteTransferOutput
+    {
+        $payee = $this->userRepo->getUserById($input->getUserPayee());
+        $walletPayee = $this->walletRepo->store($input->getWalletPayee());
+        $walletPayer = $this->walletRepo->store($input->getWalletPayer());
+        $value = $input->getValue();
+
+        if (!empty($walletPayer->getCompanyId())) {
+            throw new Exception("Somente pessoa física pode realizar transferências.");
+        }
+
+        if ($walletPayer->getId() === $walletPayee->getId()) {
+            throw new Exception("Você não pode realizar transferências para sua própria carteira.");
+        }
+
+        $executeTransfer = (new CreateTransferUsecase($this->transferRepo))->handle($input);
+
+        (new DecreaseBalanceUsecase($this->walletRepo))->handle((int) $walletPayer->getId(), $value);
+        (new IncreaseBalanceUsecase($this->walletRepo))->handle((int) $walletPayee->getId(), $value);
+
+        $validateOperation = $this->http->setBaseUrl("https://util.devi.tools/api/v2/authorize");
+        $validateOperation->get();
+
+        if ($validateOperation->getResponse()->status != "success") {
+            throw new Exception("A tranferência não pode ser concluída pela operadora, 
+                    tente novamente mais tarde.");
+        }
+
+        return new ExecuteTransferOutput([
+            "id" => $executeTransfer->getId(),
+            "payerName" => $payer->getFirstName(),
+            "payeeName" => $payee->getFirstName(),
+            "value" => $executeTransfer->getValue(),
+            "status" => $executeTransfer->getStatus(),
+            "createdAt" => $executeTransfer->getCreatedAt()
+        ]);
+    }
+}
